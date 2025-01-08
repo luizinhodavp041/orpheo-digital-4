a82aeec1a78c776bcfa45ef2cee4e7f6b74eca80
luizinhodavp041 <luizinhodavp041@gmail.com>
2025-01-07 21:01:01 -0300

    removido import button header

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export function Header() {
